49f80a98b5f08175005f355fc7a7db23934656a8
Miki <amoo_miki@yahoo.com>
2023-02-02 08:29:58 -0800

    osd test

--- bin/cli.js
+++ bin/cli.js
@@ -18,46 +18,46 @@ const startTime = Date.now();
 program.storeOptionsAsProperties();
 
 program
-	.version(version)
-	.usage('[options] <src...>')
-	.option('-b, --basedir <path>', 'base directory for resolving paths')
-	.option('-s, --summary', 'show dependency count summary')
-	.option('-c, --circular', 'show circular dependencies')
-	.option('-d, --depends <name>', 'show module dependents')
-	.option('-x, --exclude <regexp>', 'exclude modules using RegExp')
-	.option('-j, --json', 'output as JSON')
-	.option('-i, --image <file>', 'write graph to file as an image')
-	.option('-I, --interactive <file>', 'write graph to file as an interactive html page')
-	.option('-l, --layout <name>', 'layout engine to use for graph (dot/neato/fdp/sfdp/twopi/circo)')
-	.option('--orphans', 'show modules that no one is depending on')
-	.option('--leaves', 'show modules that have no dependencies')
-	.option('--dot', 'show graph using the DOT language')
-	.option('--rankdir <direction>', 'set the direction of the graph layout')
-	.option('--extensions <list>', 'comma separated string of valid file extensions')
-	.option('--require-config <file>', 'path to RequireJS config')
-	.option('--webpack-config <file>', 'path to webpack config')
-	.option('--ts-config <file>', 'path to typescript config')
-	.option('--graphviz-path <path>', 'path to GraphViz installation')
-	.option('--detective-options <string>', 'JSON string to be passed to detective')
-	.option('--include-npm', 'include shallow NPM modules', false)
-	.option('--no-color', 'disable color in output and image', false)
-	.option('--no-spinner', 'disable progress spinner', false)
-	.option('--stdin', 'read predefined tree from STDIN', false)
-	.option('--warning', 'show warnings about skipped files', false)
-	.option('--debug', 'turn on debug output', false)
-	.parse(process.argv);
+  .version(version)
+  .usage('[options] <src...>')
+  .option('-b, --basedir <path>', 'base directory for resolving paths')
+  .option('-s, --summary', 'show dependency count summary')
+  .option('-c, --circular', 'show circular dependencies')
+  .option('-d, --depends <name>', 'show module dependents')
+  .option('-x, --exclude <regexp>', 'exclude modules using RegExp')
+  .option('-j, --json', 'output as JSON')
+  .option('-i, --image <file>', 'write graph to file as an image')
+  .option('-I, --interactive <file>', 'write graph to file as an interactive html page')
+  .option('-l, --layout <name>', 'layout engine to use for graph (dot/neato/fdp/sfdp/twopi/circo)')
+  .option('--orphans', 'show modules that no one is depending on')
+  .option('--leaves', 'show modules that have no dependencies')
+  .option('--dot', 'show graph using the DOT language')
+  .option('--rankdir <direction>', 'set the direction of the graph layout')
+  .option('--extensions <list>', 'comma separated string of valid file extensions')
+  .option('--require-config <file>', 'path to RequireJS config')
+  .option('--webpack-config <file>', 'path to webpack config')
+  .option('--ts-config <file>', 'path to typescript config')
+  .option('--graphviz-path <path>', 'path to GraphViz installation')
+  .option('--detective-options <string>', 'JSON string to be passed to detective')
+  .option('--include-npm', 'include shallow NPM modules', false)
+  .option('--no-color', 'disable color in output and image', false)
+  .option('--no-spinner', 'disable progress spinner', false)
+  .option('--stdin', 'read predefined tree from STDIN', false)
+  .option('--warning', 'show warnings about skipped files', false)
+  .option('--debug', 'turn on debug output', false)
+  .parse(process.argv);
 
 if (!program.args.length && !program.stdin) {
-	console.log(program.helpInformation());
-	process.exit(1);
+  console.log(program.helpInformation());
+  process.exit(1);
 }
 
 if (program.debug) {
-	process.env.DEBUG = '*';
+  process.env.DEBUG = '*';
 }
 
 if (!program.color) {
-	process.env.DEBUG_COLORS = false;
+  process.env.DEBUG_COLORS = false;
 }
 
 const log = require('../lib/log');
@@ -66,23 +66,23 @@ const madge = require('../lib/api');
 
 let packageConfig = {};
 try {
-	packageConfig = require(path.join(process.cwd(), 'package.json')).madge;
+  packageConfig = require(path.join(process.cwd(), 'package.json')).madge;
 } catch (e) { }
 const config = Object.assign(rc, packageConfig);
 
 program.options.forEach((opt) => {
-	const name = opt.name();
+  const name = opt.name();
 
-	if (program[name]) {
-		config[name] = program[name];
-	}
+  if (program[name]) {
+    config[name] = program[name];
+  }
 });
 
 const spinner = ora({
-	text: 'Finding files',
-	color: 'white',
-	interval: 100000,
-	isEnabled: program.spinner
+  text: 'Finding files',
+  color: 'white',
+  interval: 100000,
+  isEnabled: program.spinner
 });
 
 let exitCode = 0;
@@ -92,231 +92,246 @@ delete config.config;
 delete config.configs;
 
 if (rc.config) {
-	log('using runtime config %s', rc.config);
+  log('using runtime config %s', rc.config);
 }
 
 if (program.basedir) {
-	config.baseDir = program.basedir;
+  config.baseDir = program.basedir;
 }
 
 if (program.exclude) {
-	config.excludeRegExp = [program.exclude];
+  config.excludeRegExp = [program.exclude];
 }
 
 if (program.extensions) {
-	config.fileExtensions = program.extensions.split(/[, ]/).map((s) => s.trim());
+  config.fileExtensions = program.extensions.split(/[, ]/).map((s) => s.trim());
 }
 
 if (program.requireConfig) {
-	if (fs.existsSync(program.requireConfig)) {
-		config.requireConfig = program.requireConfig;
-	} else {
-		console.log(chalk.red(`\nError: Cannot find ${program.requireConfig}`));
-		process.exit(2);
-	}
+  if (fs.existsSync(program.requireConfig)) {
+    config.requireConfig = program.requireConfig;
+  } else {
+    console.log(chalk.red(`\nError: Cannot find ${program.requireConfig}`));
+    process.exit(2);
+  }
 }
 
 if (program.webpackConfig) {
-	if (fs.existsSync(program.webpackConfig)) {
-		config.webpackConfig = program.webpackConfig;
-	} else {
-		console.log(chalk.red(`\nError: Cannot find ${program.webpackConfig}`));
-		process.exit(2);
-	}
+  if (fs.existsSync(program.webpackConfig)) {
+    config.webpackConfig = program.webpackConfig;
+  } else {
+    console.log(chalk.red(`\nError: Cannot find ${program.webpackConfig}`));
+    process.exit(2);
+  }
 }
 
 if (program.tsConfig) {
-	if (fs.existsSync(program.tsConfig)) {
-		config.tsConfig = program.tsConfig;
-	} else {
-		console.log(chalk.red(`\nError: Cannot find ${program.tsConfig}`));
-		process.exit(2);
-	}
+  if (fs.existsSync(program.tsConfig)) {
+    config.tsConfig = program.tsConfig;
+  } else {
+    console.log(chalk.red(`\nError: Cannot find ${program.tsConfig}`));
+    process.exit(2);
+  }
 }
 
 if (program.graphvizPath) {
-	const graphvizBinary = process.platform === 'win32' ? 'gvpr.exe' : 'gvpr';
-	if (fs.existsSync(path.join(program.graphvizPath, graphvizBinary))) {
-		config.graphVizPath = program.graphvizPath;
-	} else if (fs.existsSync(path.join(program.graphvizPath, 'bin', graphvizBinary))) {
-		config.graphVizPath = path.join(program.graphvizPath, 'bin');
-	} else {
-		console.log(chalk.red(`\nError: Cannot find ${graphvizBinary} in ${program.graphvizPath}`));
-		process.exit(2);
-	}
+  const graphvizBinary = process.platform === 'win32' ? 'gvpr.exe' : 'gvpr';
+  if (fs.existsSync(path.join(program.graphvizPath, graphvizBinary))) {
+    config.graphVizPath = program.graphvizPath;
+  } else if (fs.existsSync(path.join(program.graphvizPath, 'bin', graphvizBinary))) {
+    config.graphVizPath = path.join(program.graphvizPath, 'bin');
+  } else {
+    console.log(chalk.red(`\nError: Cannot find ${graphvizBinary} in ${program.graphvizPath}`));
+    process.exit(2);
+  }
 }
 
 if (program.detectiveOptions) {
-	try {
-		config.detectiveOptions = JSON5.parse(program.detectiveOptions);
-	} catch (ex) {
-		console.log('\n%s\n%s', chalk.red('Error: Invalid JSON string'), program.detectiveOptions);
-		process.exit(2);
-	}
+  try {
+    config.detectiveOptions = JSON5.parse(program.detectiveOptions);
+  } catch (ex) {
+    console.log('\n%s\n%s', chalk.red('Error: Invalid JSON string'), program.detectiveOptions);
+    process.exit(2);
+  }
 }
 
 if (program.includeNpm) {
-	config.includeNpm = program.includeNpm;
+  config.includeNpm = program.includeNpm;
 }
 
 if (!program.color) {
-	config.backgroundColor = '#ffffff';
-	config.nodeColor = '#000000';
-	config.noDependencyColor = '#000000';
-	config.cyclicNodeColor = '#000000';
-	config.edgeColor = '#757575';
+  config.backgroundColor = '#ffffff';
+  config.nodeColor = '#000000';
+  config.noDependencyColor = '#000000';
+  config.cyclicNodeColor = '#000000';
+  config.edgeColor = '#757575';
 }
 
 if (program.rankdir) {
-	config.rankdir = program.rankdir;
+  config.rankdir = program.rankdir;
 }
 
+config.renamePath = name => {
+  if (name.indexOf('/target/') > 0) return name.replace('/target/', '/');
+  return name;
+};
+
+config.getNodeAttributes = (file) => {
+  let group;
+  if (file.startsWith('src/plugins/')) group = file.replace(/^(src\/plugins\/[^\/]+)\/.*$/, '$1');
+  else if (file.startsWith('src/')) group = file.replace(/^(src\/[^\/]+)\/.*$/, '$1');
+  else if (file.startsWith('packages/')) group = file.replace(/^(packages\/[^\/]+)\/.*$/, '$1');
+  else console.warn('Failed grouping:', file);
+
+  return { group };
+};
+
 function dependencyFilter() {
-	let prevFile;
+  let prevFile;
 
-	return (dependencyFilePath, traversedFilePath, baseDir) => {
-		if (prevFile !== traversedFilePath) {
-			const relPath = path.relative(baseDir, traversedFilePath);
-			const dir = path.dirname(relPath) + '/';
-			const file = path.basename(relPath);
+  return (dependencyFilePath, traversedFilePath, baseDir) => {
+    if (prevFile !== traversedFilePath) {
+      const relPath = path.relative(baseDir, traversedFilePath);
+      const dir = path.dirname(relPath) + '/';
+      const file = path.basename(relPath);
 
-			if (program.spinner) {
-				spinner.text = chalk.grey(dir) + chalk.cyan(file);
-				spinner.render();
-			}
-			prevFile = traversedFilePath;
-		}
-	};
+      if (program.spinner) {
+        spinner.text = chalk.grey(dir) + chalk.cyan(file);
+        spinner.render();
+      }
+      prevFile = traversedFilePath;
+    }
+  };
 }
 
 new Promise((resolve, reject) => {
-	if (program.stdin) {
-		let buffer = '';
+  if (program.stdin) {
+    let buffer = '';
 
-		process.stdin
-			.resume()
-			.setEncoding('utf8')
-			.on('data', (chunk) => {
-				buffer += chunk;
-			})
-			.on('end', () => {
-				try {
-					resolve(JSON.parse(buffer));
-				} catch (e) {
-					reject(e);
-				}
-			});
-	} else {
-		resolve(program.args);
-	}
+    process.stdin
+      .resume()
+      .setEncoding('utf8')
+      .on('data', (chunk) => {
+        buffer += chunk;
+      })
+      .on('end', () => {
+        try {
+          resolve(JSON.parse(buffer));
+        } catch (e) {
+          reject(e);
+        }
+      });
+  } else {
+    resolve(program.args);
+  }
 })
-	.then((src) => {
-		if (!program.json && !program.dot) {
-			spinner.start();
-			config.dependencyFilter = dependencyFilter();
-		}
+  .then((src) => {
+    if (!program.json && !program.dot) {
+      spinner.start();
+      config.dependencyFilter = dependencyFilter();
+    }
 
-		return madge(src, config);
-	})
-	.then((res) => {
-		if (!program.json && !program.dot) {
-			spinner.stop();
-			output.getResultSummary(res, startTime);
-		}
+    return madge(src, config);
+  })
+  .then((res) => {
+    if (!program.json && !program.dot) {
+      spinner.stop();
+      output.getResultSummary(res, startTime);
+    }
 
-		const result = createOutputFromOptions(program, res);
-		if (result !== undefined) {
-			return result;
-		}
+    const result = createOutputFromOptions(program, res);
+    if (result !== undefined) {
+      return result;
+    }
 
-		output.list(res.obj(), {
-			json: program.json
-		});
+    output.list(res.obj(), {
+      json: program.json
+    });
 
-		return res;
-	})
-	.then((res) => {
-		if (program.warning && !program.json) {
-			output.warnings(res);
-		}
+    return res;
+  })
+  .then((res) => {
+    if (program.warning && !program.json) {
+      output.warnings(res);
+    }
 
-		if (!program.json && !program.dot) {
-			console.log('');
-		}
+    if (!program.json && !program.dot) {
+      console.log('');
+    }
 
-		process.exit(exitCode);
-	})
-	.catch((err) => {
-		spinner.stop();
-		console.log('\n%s %s\n', chalk.red('✖'), err.stack);
-		process.exit(1);
-	});
+    process.exit(exitCode);
+  })
+  .catch((err) => {
+    spinner.stop();
+    console.log('\n%s %s\n', chalk.red('✖'), err.stack);
+    process.exit(1);
+  });
 
 function createOutputFromOptions(program, res) {
-	if (program.summary) {
-		output.summary(res.obj(), {
-			json: program.json
-		});
+  if (program.summary) {
+    output.summary(res.obj(), {
+      json: program.json
+    });
 
-		return res;
-	}
+    return res;
+  }
 
-	if (program.depends) {
-		output.modules(res.depends(program.depends), {
-			json: program.json
-		});
+  if (program.depends) {
+    output.modules(res.depends(program.depends), {
+      json: program.json
+    });
 
-		return res;
-	}
+    return res;
+  }
 
-	if (program.orphans) {
-		output.modules(res.orphans(), {
-			json: program.json
-		});
+  if (program.orphans) {
+    output.modules(res.orphans(), {
+      json: program.json
+    });
 
-		return res;
-	}
+    return res;
+  }
 
-	if (program.leaves) {
-		output.modules(res.leaves(), {
-			json: program.json
-		});
+  if (program.leaves) {
+    output.modules(res.leaves(), {
+      json: program.json
+    });
 
-		return res;
-	}
+    return res;
+  }
 
-	if (program.image) {
-		return res.image(program.image, program.circular).then((imagePath) => {
-			spinner.succeed(`${chalk.bold('Image created at')} ${chalk.cyan.bold(imagePath)}`);
-			return res;
-		});
-	}
+  if (program.image) {
+    return res.image(program.image, program.circular).then((imagePath) => {
+      spinner.succeed(`${chalk.bold('Image created at')} ${chalk.cyan.bold(imagePath)}`);
+      return res;
+    });
+  }
 
-	if (program.interactive) {
-		return res.interactive(program.interactive, program.circular).then((pagePath) => {
-			spinner.succeed(`${chalk.bold('Html page created at')} ${chalk.cyan.bold(pagePath)}`);
-			return res;
-		});
-	}
+  if (program.interactive) {
+    return res.interactive(program.interactive, program.circular).then((pagePath) => {
+      spinner.succeed(`${chalk.bold('Html page created at')} ${chalk.cyan.bold(pagePath)}`);
+      return res;
+    });
+  }
 
-	if (program.dot) {
-		return res.dot(program.circular).then((output) => {
-			process.stdout.write(output);
-			return res;
-		});
-	}
+  if (program.dot) {
+    return res.dot(program.circular).then((output) => {
+      process.stdout.write(output);
+      return res;
+    });
+  }
 
-	if (program.circular) {
-		const circular = res.circular();
+  if (program.circular) {
+    const circular = res.circular();
 
-		output.circular(spinner, res, circular, {
-			json: program.json
-		});
+    output.circular(spinner, res, circular, {
+      json: program.json
+    });
 
-		if (circular.length) {
-			exitCode = 1;
-		}
+    if (circular.length) {
+      exitCode = 1;
+    }
 
-		return res;
-	}
+    return res;
+  }
 }
